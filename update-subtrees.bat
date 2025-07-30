994ce16713b20ed44f7eb4af0194f8e4be92458d
Minq05 <hoangquan22022005@gmail.com>
2025-07-30 14:02:12 +0700

    Update script to preserve full commit history

--- update-subtrees.bat
+++ update-subtrees.bat
@@ -3,15 +3,15 @@ echo Updating subtrees...
 
 echo.
 echo Updating client...
-git subtree pull --prefix=client https://github.com/thenam2kx/Graduation_Project-client.git main --squash
+git subtree pull --prefix=client https://github.com/thenam2kx/Graduation_Project-client.git main
 
 echo.
 echo Updating admin...
-git subtree pull --prefix=admin https://github.com/thenam2kx/Graduation_Project-admin.git main --squash
+git subtree pull --prefix=admin https://github.com/thenam2kx/Graduation_Project-admin.git main
 
 echo.
 echo Updating backend...
-git subtree pull --prefix=backend https://github.com/thenam2kx/Graduation_Project-APIs.git main --squash
+git subtree pull --prefix=backend https://github.com/thenam2kx/Graduation_Project-APIs.git main
 
 echo.
 echo All subtrees updated successfully!
